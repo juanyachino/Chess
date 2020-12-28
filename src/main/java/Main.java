08dd65bd26eaa12f5add68159ad939f896971cf3
juanyachino <juanyachino@gmail.com>
2020-12-28 17:30:06 -0300

    pom setup and spark examples

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+        get("/hello/:name", (request, response) -> {
+            return "Hello: " + request.params(":name");
+        });
+    }
+}
